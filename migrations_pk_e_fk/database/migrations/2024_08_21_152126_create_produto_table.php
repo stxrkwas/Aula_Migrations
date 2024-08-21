9a665be785c17962a673562749c446541762f32b
stxrkwas <malusilva0918.etec@gmail.com>
2024-08-21 12:29:17 -0300

    Acabei, por enquanto

---
+++ migrations_pk_e_fk/database/migrations/2024_08_21_152126_create_produto_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produto', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 50);
+            $table->text('descricao');
+            $table->integer('peso');
+            $table->integer('estoque');
+            $table->float('preco');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produto');
+    }
+};
---
+++ migrations_pk_e_fk/database/migrations/2024_08_21_152506_create_produto_detalhes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produto_detalhes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('produto_id');
+            $table->float();
+            $table->id();
+            $table->id();
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produto_detalhes');
+    }
+};
